f58bf21bdfd674ae1287d3eef121287e0fc3e0d3
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-03-09 23:52:13 +0530

    feat: add more route to add, update, delete by ID of the exercise

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -27,4 +27,32 @@ exerciseRouter.route("/add").post((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+exerciseRouter.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.status(200).json(exercise))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+exerciseRouter.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).json("Exercise deleted"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+exerciseRouter.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.status(200).json("Exercise Updated"))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
 module.exports = exerciseRouter;
