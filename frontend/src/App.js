2c2430365f03cf13737a663b3af1cc8723217024
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-03-10 11:18:47 +0530

    feat: add new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar.component";
+import ExercisesList from "./components/ExerciseList.component";
+import EditExercise from "./components/EditExercise.component";
+import CreateExercise from "./components/CreateExercise.component";
+import CreateUser from "./components/CreateUser.component";
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <div className="container">
         <Navbar />
         <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
